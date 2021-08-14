2b3011eabd05a01c205da5ca789e2cdbd3340e59
Assaad GHABI <ghabiassaad@gmail.com>
2021-08-14 17:42:51 +0100

    oops

---
+++ src/components/fr/csharp/Oop.js
@@ -0,0 +1,169 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Oop() {
+  return (
+    <>
+      <aside className="bd-aside sticky-xl-top text-muted align-self-start mb-3 mb-xl-5 px-2">
+        <h2 className="h6 pt-4 pb-3 mb-4 border-bottom">On this page</h2>
+        <nav className="small" id="toc">
+          <ul className="list-unstyled">
+            <li className="my-2">
+              <button
+                className="btn d-inline-flex align-items-center"
+                data-bs-toggle="collapse"
+                aria-expanded="true"
+                data-bs-target="#contents-collapse"
+                aria-controls="contents-collapse"
+              >
+                Contents
+              </button>
+              <ul
+                className="list-unstyled ps-3 collapse show"
+                id="contents-collapse"
+              >
+                <li>
+                  <a
+                    className="d-inline-flex align-items-center rounded"
+                    href="#introduction"
+                  >
+                    Introduction
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="d-inline-flex align-items-center rounded"
+                    href="#inheritance"
+                  >
+                    Inheritance
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="d-inline-flex align-items-center rounded"
+                    href="#abstraction"
+                  >
+                    Abstraction
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="d-inline-flex align-items-center rounded"
+                    href="#polymorphism"
+                  >
+                    Polymorphism
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="d-inline-flex align-items-center rounded"
+                    href="#encapsulation"
+                  >
+                    encapsulation
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="my-2">
+              <button
+                className="btn d-inline-flex align-items-center"
+                data-bs-toggle="collapse"
+                aria-expanded="true"
+                data-bs-target="#contents-collapse"
+                aria-controls="contents-collapse"
+              >
+                Related links
+              </button>
+              <ul
+                className="list-unstyled ps-3 collapse show"
+                id="contents-collapse"
+              >
+                <li>
+                  <Link
+                    className="d-inline-flex align-items-center rounded"
+                    to="/entityframework"
+                  >
+                    Entity Framework
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+      <div className="bd-cheatsheet container-fluid bg-body">
+        <section id="dotnet-core">
+          <h2 className="sticky-xl-top fw-bold p-0 m-0">Contents</h2>
+          <h3>Object-Oriented Programming</h3>
+          <article className="" id="introduction">
+            <h6>Introduction</h6>
+            <div>
+              <p>
+                La programmation procédurale consiste à écrire des procédures ou
+                des méthodes qui effectuent des opérations sur les données,
+                tandis que la programmation orientée objet consiste à créer des
+                objets qui contiennent à la fois des données et des méthodes.
+              </p>
+              <ul>
+                <li>La POO est plus rapide et plus facile à exécuter</li>
+                <li>La POO fournit une structure claire pour les programmes</li>
+                <li>
+                  La POO facilite la maintenance, la modification et le débogage
+                  du code
+                </li>
+                <li>
+                  La POO permet de créer des applications entièrement
+                  réutilisables avec moins de code et un temps de développement
+                  plus court
+                </li>
+              </ul>
+            </div>
+          </article>
+          <article className="" id="inheritance">
+            <h6>Héritage</h6>
+            <p>L'héritage est utile pour la réutilisabilité du code.</p>
+          </article>
+          <article className="" id="abstraction">
+            <h6>L'abstraction</h6>
+            <p>
+              Pour assurer la sécurité, masquez certains détails et n'affichez
+              que les détails importants d'un objet.L'abstraction peut être
+              réalisée avec des classes abstraites ou des interfaces.
+            </p>
+          </article>
+          <article className="" id="polymorphism">
+            <h6>Polymorphisme</h6>
+            <p>
+              Le polymorphisme signifie "plusieurs formes", et il se produit
+              lorsque nous avons de nombreuses classes qui sont liées les unes
+              aux autres par héritage. Il est utile pour la réutilisabilité du
+              code.
+            </p>
+          </article>
+          <article className="" id="encapsulation">
+            <h6>Properties and Encapsulation</h6>
+            <div>
+              <p>
+                Le sens de l'
+                <strong>Encapsulation</strong>, est de s'assurer que les données
+                "sensibles" sont cachées aux utilisateurs. Pour y parvenir, vous
+                devez :
+              </p>
+              <ul>
+                <li>
+                  déclarer les champs/variables comme <code>private</code>
+                </li>
+                <li>
+                  fournir <code>public</code> <code>get</code> et des{" "}
+                  <code>set</code> méthodes, via des <strong>propriétés</strong>
+                  , pour accéder et mettre à jour la valeur d'un{" "}
+                  <code>private</code> champ.
+                </li>
+              </ul>
+            </div>
+          </article>
+        </section>
+      </div>
+    </>
+  );
+}
