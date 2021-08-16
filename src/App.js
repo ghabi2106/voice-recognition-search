7c14395395f2941c9c1887b60f4ef11fe648bde3
Assaad GHABI <ghabiassaad@gmail.com>
2021-08-16 08:51:09 +0100

    app

--- src/App.js
+++ src/App.js
@@ -184,75 +184,92 @@ function App() {
                   {
                     //#region dotnet
                   }
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/clean">
-                      Clean
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/clr">
-                      CLR
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/controller">
-                      Controller
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/controllervuedata">
-                      Controller-Vue-Data
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/cqrs">
-                      CQRS
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/designpattern">
-                      Design-Pattern
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/di">
-                      DI
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/dotnet">
-                      Dotnet
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/entityframework">
-                      EF
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/httpmethods">
-                      Http-Methods
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/mvc">
-                      MVC
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/nugetpackages">
-                      Nuget-Packages
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/service">
-                      Service
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/singleton">
-                      Singleton
-                    </Link>
+                  <li className="nav-item dropdown">
+                    <a
+                      className="nav-link dropdown-toggle"
+                      href="#"
+                      id="navbarDropdown"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      .Net
+                    </a>
+                    <ul
+                      className="dropdown-menu"
+                      aria-labelledby="navbarDropdown"
+                    >
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/clean">
+                          Clean
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/clr">
+                          CLR
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/controller">
+                          Controller
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/controllervuedata">
+                          Controller-Vue-Data
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/cqrs">
+                          CQRS
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/designpattern">
+                          Design-Pattern
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/di">
+                          DI
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/dotnet">
+                          Dotnet
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/entityframework">
+                          EF
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/httpmethods">
+                          Http-Methods
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/mvc">
+                          MVC
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/nugetpackages">
+                          Nuget-Packages
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/service">
+                          Service
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/singleton">
+                          Singleton
+                        </Link>
+                      </li>
+                    </ul>
                   </li>
                   {
                     //#endregion
@@ -261,75 +278,89 @@ function App() {
                   {
                     //#region csharp
                   }
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/boxing">
-                      Boxing
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/collections">
-                      Collections
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/covariance">
-                      Covariance
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/datatype">
-                      Datatype
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/delegate">
-                      Delegate
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/enumindexer">
-                      Enumindexer
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/modifiers">
-                      Modifiers
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/csharp-features">
-                      C#-Features
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/oop">
-                      OOP
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/statements">
-                      Statements
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/string2int">
-                      String2Int
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/stringbuilder">
-                      String-Builder
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/tuple">
-                      Tuple
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/typesystem">
-                      Type-System
-                    </Link>
+                  <li class="nav-item dropdown">
+                    <a
+                      class="nav-link dropdown-toggle"
+                      href="#"
+                      id="navbarDropdown"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Csharp
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/boxing">
+                          Boxing
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/collections">
+                          Collections
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/covariance">
+                          Covariance
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/datatype">
+                          Datatype
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/delegate">
+                          Delegate
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/enumindexer">
+                          Enumindexer
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/modifiers">
+                          Modifiers
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/csharp-features">
+                          C#-Features
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/oop">
+                          OOP
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/statements">
+                          Statements
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/string2int">
+                          String2Int
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/stringbuilder">
+                          String-Builder
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/tuple">
+                          Tuple
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="dropdown-item" to="/typesystem">
+                          Type-System
+                        </Link>
+                      </li>
+                    </ul>
                   </li>
                   {
                     //#endregion
